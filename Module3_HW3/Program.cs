d91d7fb5c3e33ffddccb217a9277f5b91fab8178
Yaroslav Chushenko <yarik.chushenko1@gmail.com>
2022-12-04 15:22:42 +0200

    Finished task implementation

--- Module3_HW3/Program.cs
+++ Module3_HW3/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Module3_HW3
+{
+    public delegate int PowHandler(int a, int b);
+    public delegate bool ResultHandler(int number);
+
+    public class Program
+    {
+        public static void Show(bool result)
+        {
+            Console.WriteLine(result);
+        }
+
+        public static void Main(string[] args)
+        {
+            Class1.Show showDelegate = Program.Show;
+            ResultHandler resultHandler = Class2.Calc(Class1.Pow, 3, 9);
+            showDelegate(resultHandler(3));
+        }
+    }
+}
